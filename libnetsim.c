49bf216d7790dd9a32220e4b43a17b52ffee3377
Rodolfo Vick <rodolfo.vick@gmail.com>
2015-08-04 23:03:42 -0300

    Waiting the packet time.

--- libnetsim.c
+++ libnetsim.c
@@ -9,6 +9,44 @@
  * Network simulation using a pcap file.
  */
 
+/* Wait packet time */
+void ns_pkt_timer(struct timeval t, int reset) 
+{
+	static struct timeval ti;
+	struct timeval td;
+
+	static struct timeval si;
+	struct timeval sd;
+	struct timeval s;
+
+
+	if (reset) {
+		memcpy(&ti, &t, sizeof(struct timeval));
+		gettimeofday(&si, NULL);
+	}
+	
+	timersub(&t, &ti, &td);
+	do {
+		gettimeofday(&s, NULL);
+		timersub(&s, &si, &sd);
+	} while (timercmp(&sd, &td, <));
+	printf("%ld.%ld %ld.%ld ", td.tv_sec, td.tv_usec, sd.tv_sec, sd.tv_usec);
+}
+
+/* Read packet callback */
+void ns_pkt_read(u_char *user, const struct pcap_pkthdr *h, const u_char *pkt) 
+{
+	unsigned int i;
+	int *reset = (int *)user;
+
+	ns_pkt_timer(h->ts, *reset);
+	*reset = 0;
+ 
+	for (i = 0; i < h->caplen; i++)
+		printf("%02x ", pkt[i]);
+	printf("\n\n");
+}
+
 /**
  * opens a PCAP file and returs the result.
  * @param file name string.
@@ -28,16 +66,6 @@ int netsim_init(char *name, pcap_t **pcap)
 	return NS_OK;
 }
 
-/* Read packet callback */
-void ns_pkt_read(u_char *user, const struct pcap_pkthdr *h, const u_char *pkt) 
-{
-	unsigned int i;
-
-	for (i = 0; i < h->caplen; i++)
-		printf("%02x ", pkt[i]);
-	printf("\n\n");
-}
-
 /**
  * start the simulation.
  * @param the opened file pcap struct pointer.
@@ -46,5 +74,6 @@ void ns_pkt_read(u_char *user, const struct pcap_pkthdr *h, const u_char *pkt)
  */ 
 int netsim_start(pcap_t *pcap, unsigned char loop)
 {
-	return pcap_dispatch(pcap, 0, ns_pkt_read, NULL);
+	int reset = 1;
+	return pcap_dispatch(pcap, 0, ns_pkt_read, (u_char *)&reset);
 }
